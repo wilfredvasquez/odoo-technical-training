c5848370587ac9d056f7c39586ca0bf70b6258d5
Wilfred Vásquez <wilfredvas@gmail.com>
2023-11-06 20:18:11 -0400

    motorcycle_registry module was created

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Motorcycle Registry",
+    "summary": """Manage Registration of Motorcycles""",
+    "description": """
+        Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+    """,
+    "version": "0.0.1",
+    "author": "wilfredvasquez",
+    "license": "OPL-1",
+    "category": "Project",
+    "website": "https://github.com/wilfredvasquez",
+    "company": "Bitmotto",
+    "maintainer": "wilfredvasquez",
+    "application": True,
+    "category": "Kawiil/Motorcycle",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+}
