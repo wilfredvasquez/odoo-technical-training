da4f2d76eae14b2f6e4690ae9f98250b44140045
Wilfred Vásquez <wilfredvas@gmail.com>
2023-11-06 22:40:48 -0400

    Set registry_number field as _rec_name

--- motorcycle_registry/models/motorcycle_register.py
+++ motorcycle_registry/models/motorcycle_register.py
@@ -6,12 +6,13 @@ class MotorcycleRegistry(models.Model):
 
     _name = "motorcycle.registry"
     _description = "Motorcycle Registry"
+    _rec_name = "registry_number"
 
-    name = fields.Char(string="Número de Registro", required=True)
     certificate_title = fields.Binary(string="Título de Propiedad")
     current_mileage = fields.Float(string="Millaje Actual")
     first_name = fields.Char(string="Nombre", required=True)
     last_name = fields.Char(string="Apellido", required=True)
     license_plate = fields.Char(string="Matrícula")
     registry_date = fields.Date()
+    registry_number = fields.Char(string="Número de Registro", required=True)
     vin = fields.Char(string="VIN", required=True)
