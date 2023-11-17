bfb1433256b52f208d6af2accf1bc60ba8d51883
Wilfred Vásquez <wilfredvas@gmail.com>
2023-11-17 10:23:50 -0400

    compute name field of product.template model

--- motorcycle_stock/models/product_template.py
+++ motorcycle_stock/models/product_template.py
@@ -1,9 +1,19 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    name = fields.Char(
+        "Name",
+        index="trigram",
+        required=True,
+        translate=True,
+        compute="_compute_name",
+        precompute=True,
+        store=True,
+        readonly=True,
+    )
     detailed_type = fields.Selection(
         selection_add=[
             ("motorcycle", "Motorcycle"),
@@ -39,3 +49,12 @@ class ProductTemplate(models.Model):
         return dict(self._fields["battery_capacity"].selection).get(
             self.battery_capacity
         )
+
+    @api.depends("make", "model", "year")
+    def _compute_name(self):
+        for rec in self:
+            if rec.detailed_type == "motorcycle":
+                year = rec.year if rec.year else " "
+                make = rec.make if rec.make else " "
+                model = rec.model if rec.model else " "
+                rec.name = f"{rec.year} {rec.make} {rec.model}"
